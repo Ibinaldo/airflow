6f62d3bb75b4b3842fab737c7d03ce05f9b00fdd
Ibinabo Bestmann <ibi.best@gmail.com>
2023-07-24 16:16:42 -0700

    added sample dag

---
+++ dags/hello_world.py
@@ -0,0 +1,35 @@
+from airflow.decorators import dag
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+@dag(
+    "hello_world",
+    default_args=(
+        "owner": "Ibinaldo",
+        "depends_on_past": False,
+        "email": "ibi.best@gmail.com"
+        "email_on_failure": True,
+        "email_on_retry": False,
+        "retries": 2,
+        "retry_delay": timedelta(minutes=2),
+        "start_date": datetime(2023,01,01)
+    ),
+    description="DAG to validate git sync",
+    schedule_interval="@hourly"
+
+)
+
+def hello_world_dag():
+    task_start= DummyOperator(task_id="task_start")
+    task_end = DummyOperator(task_id="task_end")
+
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+
+    )
+
+    task_start >> t1 >> task_end
+
+dag = hello_world_dag()
